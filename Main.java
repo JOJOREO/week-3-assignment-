a876e550bad52e11a4cfdb65c779d97ed902fef2
Youssef <yousseif420@gmail.com>
2021-03-19 01:54:52 +0200

    try1

--- Main.java
+++ Main.java
@@ -2,25 +2,12 @@ package com.company;
 
 public class Main {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     public static void main(String[] args) {
 	System.out.Println("hello");
-  
 
 
 
-}
->>>>>>> df903e0d3c51d6b2af7aca9e096101ad368b785f
-=======
-    public static void main(String[] args)
-    {
-
-
-System.Out.Println("hello");
-System.Out.Println("world");
-System.Out.println("part 2");
 
 }
->>>>>>> branch1
 }
