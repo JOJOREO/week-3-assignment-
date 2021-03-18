d01372d602aaf1fb1348580c3ef85021c6f9c6dd
Youssef <yousseif420@gmail.com>
2021-03-19 01:33:23 +0200

    ashtr shtoor

--- Main.java
+++ Main.java
@@ -2,7 +2,13 @@ package com.company;
 
 public class Main {
 
+<<<<<<< HEAD
     public static void main(String[] args) {
 	System.out.Println("hello");
-    }
+  
+
+
+
+}
+>>>>>>> df903e0d3c51d6b2af7aca9e096101ad368b785f
 }
